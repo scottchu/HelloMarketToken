8b197d0c9e5f369812d20274f84e9a8f02f5c217
Scott Chu <scottchu@icloud.com>
2018-06-10 02:02:50 -0400

    add eth-gas-reporter for truffle test

--- truffle.js
+++ truffle.js
@@ -3,7 +3,16 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*",
+      gas: 1000000,
+      gasPrice: 10000000000
+    }
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions : {
+      currency: "CAD",
+      gasPrice: 21
     }
   }
-};
+}
