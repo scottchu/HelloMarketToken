be17f6c1d9cc8cc2104869fe612ec07d390e731f
Scott Chu <scottchu@icloud.com>
2018-06-10 02:03:20 -0400

    add contract MintableToken and tests

---
+++ contracts/MintableToken.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+import "./BasicToken.sol";
+import "./Ownable.sol";
+
+contract MintableToken is BasicToken, Ownable {
+  event Mint(address indexed to, uint256 amount);
+
+  constructor(uint _initialSupply)
+    BasicToken(_initialSupply)
+    public {}
+
+  function mint(address _to, uint256 _amount) onlyOwner public {
+    _totalSupply = _totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+
+    emit Mint(_to, _amount);
+    emit Transfer(address(0), _to, _amount);
+  }
+}
---
+++ test/MintableToken.test.js
@@ -0,0 +1,89 @@
+const shouldRevert = require("./helpers/shouldRevert")
+
+const MintableToken = artifacts.require("MintableToken")
+
+contract("MintableToken", (accounts) => {
+
+  const alice = accounts[0]
+  const bob = accounts[1]
+
+  describe("mint", () => {
+    it("prevents non-owner from mint", async () => {
+      const initialSupply = 0
+      const token = await MintableToken.new(initialSupply, { from: alice })
+
+      const mintSupply = 100
+      const q = token.mint(alice, mintSupply, { from: bob })
+
+      assert(await shouldRevert(q),
+        "bob should not be able to mint new tokens")
+    })
+
+    it("adds newly minted tokens to total supply", async () => {
+      const initialSupply = 0
+      const token = await MintableToken.new(initialSupply, { from: alice })
+
+      const mintSupply = 100
+      await token.mint(alice, mintSupply, { from: alice })
+
+      const totalSupply = await token.totalSupply()
+
+      assert(totalSupply == (initialSupply + mintSupply),
+        "total supply should increase by new minted token amount")
+    })
+
+    it("adds newly minted token to the recipient", async () => {
+      const initialSupply = 0
+      const token = await MintableToken.new(initialSupply, { from: alice })
+
+      const mintSupply = 100
+      await token.mint(bob, mintSupply, { from: alice })
+
+      const balance = await token.balanceOf(bob)
+
+      assert(balance == (initialSupply + mintSupply),
+        "bob should receive new minted tokens")
+    })
+
+    it("emits a Mint event", async () => {
+      const initialSupply = 0
+      const token = await MintableToken.new(initialSupply, { from: alice })
+
+      const mintSupply = 100
+      const { logs } = await token.mint(bob, mintSupply, { from: alice })
+
+      const log = logs[0]
+
+      assert(log.event == "Mint",
+        "log event should be Mint")
+
+      assert(log.args.to == bob,
+        "log should record bob as the the recipient")
+
+      assert(log.args.amount == mintSupply,
+        "log should record the amount")
+    })
+
+    it("emits a Transfer event", async () => {
+      const initialSupply = 0
+      const token = await MintableToken.new(initialSupply, { from: alice })
+
+      const mintSupply = 100
+      const { logs } = await token.mint(bob, mintSupply, { from: alice })
+
+      const log = logs[1]
+
+      assert(log.event == "Transfer",
+        "log event should be Transfer")
+
+      assert(log.args.from == 0,
+        "log should record Address<0> as the sender")
+
+      assert(log.args.to == bob,
+        "log should record bob as the the recipient")
+
+      assert(log.args.value == mintSupply,
+        "log should record the transfer value")
+    })
+  })
+})
